f4e9b0f501d75146f3bdd57ad51c77d3e957d1f5
Cristian <do.cristian.tr@hotmail.com>
2024-08-01 13:39:24 -0400

    feat: hero & projects section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "auto-fill-100": "repeat(auto-fill, minmax(100px, 1fr))",
+        "auto-fit-100": "repeat(auto-fit, minmax(100px, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
