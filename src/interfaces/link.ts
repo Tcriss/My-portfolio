30146c8e6ef0eb4e9446370063efcbb45c4aa75c
tcriss <do.cristian.tr@hotmail.com>
2024-09-01 11:53:03 -0400

    feat: scroll-spy

--- src/interfaces/link.ts
+++ src/interfaces/link.ts
@@ -1,5 +1,6 @@
 export interface Link {
     en_name: string;
     es_name: string;
+    label: string;
     path: string;
 }
---
+++ src/scripts/scroll-spy.js
@@ -0,0 +1,41 @@
+document.addEventListener("astro:page-load", () => {
+  const sections = document.querySelectorAll("section");
+  const navItems = document.querySelectorAll("nav ul li a");
+
+  const callback = (entries) => {
+    console.log('entries', entries);
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navItems.forEach((item) => {
+          if (item.getAttribute("aria-label") == entry.target.id) {
+            item.classList.add("text-blue-400");
+            item.classList.add('bg-slate-100');
+          } else {
+            item.classList.remove("text-blue-400");
+            item.classList.remove('bg-slate-100');
+          }
+        });
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3,
+  });
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  document.onvisibilitychange = () => {
+    if (document.visibilityState === "hidden") {
+      observer.disconnect();
+    } else {
+      sections.forEach((section) => {
+        observer.observe(section);
+      });
+    }
+  };
+});
