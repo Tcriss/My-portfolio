c6ffd9e3bdf17ea079bfafdf0c296defacc42bff
tcriss <do.cristian.tr@hotmail.com>
2024-09-18 10:34:51 -0400

    fix: settings menu alingment

--- src/scripts/select-language.js
+++ src/scripts/select-language.js
@@ -1,26 +1,25 @@
 const languageBtn = document.getElementById('language-btn');
 const settingsBtn = document.getElementById('settings-btn');
 const settingsMenu = document.getElementById('settings-menu');
-const languageList = document.getElementById('language-picker'); 
+const languageList = document.getElementById('language-picker');
 
+document.addEventListener('click', (e) => {
+    e.stopPropagation();
+    settingsMenu.classList.add('invisible');
+    settingsMenu.classList.add('-translate-y-8');
+    settingsMenu.classList.add('opacity-0');
+});
 settingsBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    settingsMenu.classList.toggle('invisible');
-    settingsMenu.classList.toggle('-translate-y-8');
-    settingsMenu.classList.toggle('opacity-0');
-});
-
-settingsBtn.addEventListener('mouseenter', (e) => {
-    e.preventDefault();
+    e.stopPropagation();
     settingsMenu.classList.toggle('invisible');
     settingsMenu.classList.toggle('-translate-y-8');
     settingsMenu.classList.toggle('opacity-0');
 });
 
 languageBtn.addEventListener('click', (e) => {
-    //Oe.preventDefault();
+    e.stopPropagation();
     languageList.classList.toggle('invisible');
     languageList.classList.toggle('-translate-x-[24px]');
     languageList.classList.toggle('opacity-0');
-    
 });
