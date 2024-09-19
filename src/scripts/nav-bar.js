f9d9f25817ace5d37b67bddeadc099ea806349c2
tcriss <do.cristian.tr@hotmail.com>
2024-09-18 23:50:24 -0400

    chores: improving design

--- src/scripts/nav-bar.js
+++ src/scripts/nav-bar.js
@@ -4,14 +4,14 @@ document.addEventListener('scroll', () => {
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
         navbar.classList.add('shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]');
         navbar.classList.add('bg-white/80');
-        navbar.classList.add('dark:bg-slate-700/70');
-        navbar.classList.add('backdrop-blur-xl');
+        navbar.classList.add('dark:bg-slate-700/90');
+        navbar.classList.add('backdrop-blur-[20px');
         navbar.classList.add('dark:border-gray-700');
     } else {
         navbar.classList.remove('shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]');
         navbar.classList.remove('bg-white/80');
-        navbar.classList.remove('dark:bg-slate-700/70');
-        navbar.classList.remove('backdrop-blur-xl');
+        navbar.classList.remove('dark:bg-slate-700/90');
+        navbar.classList.remove('backdrop-blur-[20px');
         navbar.classList.remove('dark:border-gray-700');
     }
 });
